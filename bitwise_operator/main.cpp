1a33744967d8ddb6840c6ecdf16235fd44fc859f
Fernando Mr. Serp <shelepov.1995.sergey@mail.ru>
2023-03-26 14:21:23 +0500

    Add project files.

---
+++ bitwise_operator/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	int a, b;
+
+	cout << "Введите два числа: "; cin >> a,cin >> b;
+	cout << a << " " << b << endl;
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int i = 0; i < b; i++)
+		{
+			a ^= b;
+			b ^= a;
+			a ^= b;
+		}
+	}
+	cout << a << " " << b;
+}
